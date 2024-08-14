5a3660ee016d280086d10de2ff2bbc458a190dd2
Douglas Bello <dgbello.dev@hotmail.com>
2024-08-14 13:31:11 -0300

    added json and html servlet

--- src/main/java/com/cinelits/ms/Main.java
+++ src/main/java/com/cinelits/ms/Main.java
@@ -1,7 +1,22 @@
 package com.cinelits.ms;
 
+import com.cinelits.ms.servlets.MyServlet1;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addContext("", null);
+        Wrapper servlet = Tomcat.addServlet(ctx, "myServlet", new MyServlet1());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");
+
+        tomcat.start();
     }
 }
